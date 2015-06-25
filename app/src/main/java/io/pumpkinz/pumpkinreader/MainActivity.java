7eb4d11de018a0b6ae3cc0108bb92c4d40981acf
Agustinus Kristiadi <kristiadi7@gmail.com>
2015-06-25 20:21:14 +0700

    Change minSdk to 19, targetSdk to 22, and use appcompat

--- app/src/main/java/io/pumpkinz/pumpkinreader/MainActivity.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/MainActivity.java
@@ -1,12 +1,12 @@
 package io.pumpkinz.pumpkinreader;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
