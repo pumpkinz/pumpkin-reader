fbc876b478054a30f03d5aa57a9e8381f4582096
Agustinus Kristiadi <kristiadi7@gmail.com>
2015-06-27 22:48:53 +0700

    [UI] Lay out foundation for sidenav

--- app/src/main/java/io/pumpkinz/pumpkinreader/MainActivity.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/MainActivity.java
@@ -1,7 +1,8 @@
 package io.pumpkinz.pumpkinreader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(appBar);
     }
 
 
