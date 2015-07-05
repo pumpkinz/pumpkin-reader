e706db29b9bdd07b1e8e1d46c181b5cb640355e4
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-07-05 20:43:57 +0700

    Re-order constants for consistency

--- app/src/main/java/io/pumpkinz/pumpkinreader/etc/Constants.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/etc/Constants.java
@@ -9,11 +9,11 @@ public class Constants {
     public static final String LINK = "io.pumpkinz.pumpkinreader-Link";
 
     // Shared Preferences Constants
-    public static final String TOP_FILE_SP = "io.pumpkinz.pumpkinreader.top_sharedpref_file";
-    public static final String TOP_VAL_SP = "top_sharedpref_value";
-
     public static final String NEW_FILE_SP = "io.pumpkinz.pumpkinreader.new_sharedpref_file";
     public static final String NEW_VAL_SP = "new_sharedpref_value";
+
+    public static final String TOP_FILE_SP = "io.pumpkinz.pumpkinreader.top_sharedpref_file";
+    public static final String TOP_VAL_SP = "top_sharedpref_value";
 
     public static final String SHOW_FILE_SP = "io.pumpkinz.pumpkinreader.show_sharedpref_file";
     public static final String SHOW_VAL_SP = "show_sharedpref_value";
