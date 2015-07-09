406082c70f0e46f3e3e2aab72ca767595218080a
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-07-09 21:20:19 +0700

    1. Remove Serializable 2. Add serialVersionUID to EndOfListException class

--- app/src/main/java/io/pumpkinz/pumpkinreader/exception/EndOfListException.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/exception/EndOfListException.java
@@ -1,13 +1,13 @@
 package io.pumpkinz.pumpkinreader.exception;
 
 
-import java.io.Serializable;
-
 /**
  * Thrown when DataSource is requested to return News outside of what it can return
  * from List of NewsIds it got from SharedPreference or API request
  */
-public class EndOfListException extends IndexOutOfBoundsException implements Serializable {
+public class EndOfListException extends IndexOutOfBoundsException {
+
+    private static final long serialVersionUID = 7072219552188816963L;
 
     public EndOfListException() {
         super();
