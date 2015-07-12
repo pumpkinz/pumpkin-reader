66eb898511a0cc8bd228fd1eba3d4880c80d4212
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-07-12 19:38:05 +0700

    Try to fail the test

--- app/src/test/java/io/pumpkinz/pumpkinreader/UtilTest.java
+++ app/src/test/java/io/pumpkinz/pumpkinreader/UtilTest.java
@@ -10,7 +10,7 @@ public class UtilTest {
 
     @Test
     public void getDomainName() {
-        assertEquals("reddit.com", Util.getDomainName("https://www.reddit.com/r/funny/comments/3cz30y/one_of_the_better_things_ive_seen_at_comic_con/"));
+        assertEquals("reddit.co", Util.getDomainName("https://www.reddit.com/r/funny/comments/3cz30y/one_of_the_better_things_ive_seen_at_comic_con/"));
     }
 
 }
