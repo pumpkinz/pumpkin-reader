f05ef1ad4398df55fcce1e274fc2bf9e9b71e674
Timotius Nugroho Chandra <timotius.nc@gmail.com>
2015-08-16 16:54:33 +0700

    Use CommentParcel helper class to serialize list of Comments in NewsDetailFragment

--- app/src/main/java/io/pumpkinz/pumpkinreader/model/Comment.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/model/Comment.java
@@ -1,16 +1,15 @@
 package io.pumpkinz.pumpkinreader.model;
 
-import android.os.Parcelable;
-
-import org.parceler.Parcel;
+import android.util.Log;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.pumpkinz.pumpkinreader.util.CommentParcel;
 
-@Parcel
-public class Comment extends Item implements Serializable, Parcelable {
+
+public class Comment extends Item implements Serializable {
 
     int parent;
     List<Integer> kids;
@@ -39,6 +38,20 @@ public class Comment extends Item implements Serializable, Parcelable {
         this.expanded = true;
         this.hidden = false;
         this.level = 0;
+    }
+
+    public Comment(CommentParcel commentParcel) {
+        super(commentParcel.getId(), commentParcel.isDeleted(),
+                commentParcel.getType().toString(), commentParcel.getBy(),
+                commentParcel.getTime().getTime(), commentParcel.getText(),
+                commentParcel.isDead());
+        parent = commentParcel.getParentId();
+        kids = commentParcel.getCommentIds();
+        childComments = new ArrayList<>();
+        expanded = commentParcel.isExpanded();
+        hidden = commentParcel.isHidden();
+        level = commentParcel.getLevel();
+        allChildCount = commentParcel.getAllChildCount();
     }
 
     public int getParentId() {
@@ -118,21 +131,6 @@ public class Comment extends Item implements Serializable, Parcelable {
                 .append("\n");
 
         return sb.toString();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(android.os.Parcel dest, int flags) {
-        dest.writeInt(parent);
-        dest.writeList(kids);
-        dest.writeByte((byte) (expanded ? 1 : 0));
-        dest.writeByte((byte) (hidden ? 1 : 0));
-        dest.writeInt(level);
-        dest.writeInt(allChildCount);
     }
 
 }
