1378b31309dfaa9e6c0a5088ec7900cb452f620c
Agustinus Kristiadi <kristiadi7@gmail.com>
2015-10-30 11:33:12 +0700

    [UI] Closes #83: Use collapsing toolbar

--- app/src/main/java/io/pumpkinz/pumpkinreader/NewsCommentsActivity.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/NewsCommentsActivity.java
@@ -1,17 +1,16 @@
 package io.pumpkinz.pumpkinreader;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import org.parceler.Parcels;
 
 import io.pumpkinz.pumpkinreader.etc.Constants;
 import io.pumpkinz.pumpkinreader.model.News;
+import io.pumpkinz.pumpkinreader.util.ActionUtil;
 
 
 public class NewsCommentsActivity extends PumpkinReaderActivity {
@@ -26,9 +25,12 @@ public class NewsCommentsActivity extends PumpkinReaderActivity {
         setUpToolbar();
 
         news = Parcels.unwrap(getIntent().getParcelableExtra(Constants.NEWS));
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.news_detail_fab);
-        setUpFAB(fab);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
+        return true;
     }
 
     @Override
@@ -38,6 +40,15 @@ public class NewsCommentsActivity extends PumpkinReaderActivity {
         switch (id) {
             case android.R.id.home:
                 this.finish();
+                return true;
+            case R.id.action_open:
+                ActionUtil.open(this, news);
+                return true;
+            case R.id.action_save:
+                ActionUtil.save(this, news);
+                return true;
+            case R.id.action_share:
+                ActionUtil.share(this, news);
                 return true;
         }
 
@@ -50,29 +61,6 @@ public class NewsCommentsActivity extends PumpkinReaderActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    private void setUpFAB(FloatingActionButton fab) {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, news.getTitle());
-
-                String text;
-
-                if (news.getUrl() != null && !news.getUrl().isEmpty()) {
-                    text = news.getUrl();
-                } else {
-                    text = Constants.HN_BASE_URL + news.getId();
-                }
-
-                shareIntent.putExtra(Intent.EXTRA_TEXT, text);
-                shareIntent.setType(Constants.MIME_TEXT_PLAIN);
-
-                startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.share)));
-            }
-        });
     }
 
 }
--- app/src/main/java/io/pumpkinz/pumpkinreader/NewsDetailActivity.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/NewsDetailActivity.java
@@ -1,14 +1,14 @@
 package io.pumpkinz.pumpkinreader;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.webkit.WebView;
 
 import org.parceler.Parcels;
@@ -16,6 +16,7 @@ import org.parceler.Parcels;
 import io.pumpkinz.pumpkinreader.data.NewsDetailViewPagerAdapter;
 import io.pumpkinz.pumpkinreader.etc.Constants;
 import io.pumpkinz.pumpkinreader.model.News;
+import io.pumpkinz.pumpkinreader.util.ActionUtil;
 
 
 public class NewsDetailActivity extends PumpkinReaderActivity {
@@ -25,13 +26,13 @@ public class NewsDetailActivity extends PumpkinReaderActivity {
     private TabLayout tabLayout;
     private WebView webView;
     private NewsDetailViewPagerAdapter pagerAdapter;
-    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_news_detail);
+        setUpToolbar();
 
         news = Parcels.unwrap(getIntent().getParcelableExtra(Constants.NEWS));
 
@@ -40,9 +41,6 @@ public class NewsDetailActivity extends PumpkinReaderActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.news_detail_tab);
         setUpTab(viewPager);
-
-        fab = (FloatingActionButton) findViewById(R.id.news_detail_fab);
-        setUpFAB(fab);
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
         boolean isCommentFirst = !pref.getBoolean(Constants.CONFIG_SHOW_LINK, false);
@@ -53,12 +51,27 @@ public class NewsDetailActivity extends PumpkinReaderActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         switch (id) {
             case android.R.id.home:
                 this.finish();
+                return true;
+            case R.id.action_open:
+                ActionUtil.open(this, news);
+                return true;
+            case R.id.action_save:
+                ActionUtil.save(this, news);
+                return true;
+            case R.id.action_share:
+                ActionUtil.share(this, news);
                 return true;
         }
 
@@ -82,23 +95,14 @@ public class NewsDetailActivity extends PumpkinReaderActivity {
     private void setUpViewPager(ViewPager viewPager) {
         pagerAdapter = new NewsDetailViewPagerAdapter(getSupportFragmentManager(), getResources());
         viewPager.setAdapter(pagerAdapter);
+    }
 
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
+    private void setUpToolbar() {
+        Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(appBar);
 
-            @Override
-            public void onPageSelected(int position) {
-                if (fab != null) {
-                    fab.show();
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     private void setUpTab(ViewPager viewPager) {
@@ -107,29 +111,6 @@ public class NewsDetailActivity extends PumpkinReaderActivity {
         if (news.getUrl() == null || news.getUrl().isEmpty()) {
             tabLayout.getTabAt(NewsDetailViewPagerAdapter.TAB_COMMENTS_IDX).select();
         }
-    }
-
-    private void setUpFAB(FloatingActionButton fab) {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, news.getTitle());
-
-                String text;
-
-                if (news.getUrl() != null && !news.getUrl().isEmpty()) {
-                    text = news.getUrl();
-                } else {
-                    text = Constants.HN_BASE_URL + news.getId();
-                }
-
-                shareIntent.putExtra(Intent.EXTRA_TEXT, text);
-                shareIntent.setType(Constants.MIME_TEXT_PLAIN);
-
-                startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.share)));
-            }
-        });
     }
 
     private WebView getWebView() {
