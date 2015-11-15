e9b13207421fb4a98e3150125aaa68d01e48c8e6
Agustinus Kristiadi <kristiadi7@gmail.com>
2015-11-15 09:09:48 +0700

    [UI] Make Custom Tabs config independent to external browser

--- app/src/main/java/io/pumpkinz/pumpkinreader/SettingsActivityFragment.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/SettingsActivityFragment.java
@@ -1,15 +1,10 @@
 package io.pumpkinz.pumpkinreader;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-
-import io.pumpkinz.pumpkinreader.etc.Constants;
 
 
-public class SettingsActivityFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivityFragment extends PreferenceFragment {
 
     public SettingsActivityFragment() {
     }
@@ -18,23 +13,6 @@ public class SettingsActivityFragment extends PreferenceFragment implements Shar
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        sp.registerOnSharedPreferenceChangeListener(this);
-
-        setCustomTabsPref(sp);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        if (s.equals(Constants.CONFIG_EXTERNAL_BROWSER)) {
-            setCustomTabsPref(sharedPreferences);
-        }
-    }
-
-    private void setCustomTabsPref(SharedPreferences sp) {
-        Preference pref = getPreferenceScreen().findPreference(Constants.CONFIG_CUSTOM_TABS);
-        pref.setEnabled(sp.getBoolean(Constants.CONFIG_EXTERNAL_BROWSER, false));
     }
 
 }
